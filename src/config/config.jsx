25d2744337af71e203cbe756ef4e700d41c1f7c3
henryjoshua1974 <henryfreelancing@gmail.com>
2024-09-20 10:52:07 +0530

    updation on 20-Sep-2024

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,9 @@
 const config = {
-    apiUrl: 'https://inventory.lucidpos.com/InventoryTallyAPI',
-    // otherConfig: 'value',
+  // Live URL
+  // apiUrl: 'https://inventory.lucidpos.com/InventoryTallyAPI',
+  // Testing URL
+  apiUrl: 'https://link.lucidits.com/InventoryTallyAPI',
+  // otherConfig: 'value',
   };
   
   export default config;
